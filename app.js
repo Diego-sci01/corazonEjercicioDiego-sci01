d9d6af0d8f086eb857442a820c515d343a9c0dd6
Diego-sci01 <diegosantosantana2008@gmail.com>
2025-09-26 11:52:39 +0000

    ejercicoCorazon

---
+++ app.js
@@ -0,0 +1,61 @@
+/* 
+Funciones a elaborar:
+
+* printByLine: 
+    Esta función debe imprimir el corazón línea por línea en la consola del editor, debe de haber un salto de línea entre cada impresión. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de línea.
+
+* printByChart: 
+    Esta función debe imprimir el corazón carácter por carácter en la consola del editor, debe de haber un delay entre cada impresión de carácter. 
+    Cada vez que se complete la impresión de una línea, se debe hacer un salto de línea. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de carácter.
+
+Pistas:
+
+- Investiga sobre la función setTimeout, puede ser de interés.
+- Usa console.log o process.stdout.write para imprimir en la consola de acuerdo a lo que necesites.
+- Recuerda que necesitas iterar un string, ¿cómo lo conviertes en un array de caracteres?
+
+Para iniciar el proyecto, ejecuta node app.js
+
+*/
+
+const asciiHeart = [
+    "⣠⣤⣶⣶⣦⣄⡀  ⠀⢀⣤⣴⣶⣶⣤⣀",
+    "⣼⣿⣿⣿⣿⣿⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣧",
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏",
+    " ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋ ",
+    "   ⠙⢿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀",
+    "     ⠉⢿⣿⣿⣿⠟⠋⠀",
+    "⠀      ⠙⠻⠁"
+]
+
+
+
+const printByChart = (ms) => {
+  let delay = 0;
+  for(const linea of asciiHeart){
+    for (const caracter of linea){
+        setTimeout(()=> process.stdout.write(caracter),delay);
+        delay += ms;
+    }
+    setTimeout(()=> process.stdout.write("\n"), delay);
+  }
+};
+
+
+ const printByLine = (ms) => {
+ for (let i= 0; i < asciiHeart.length; i++) {
+       setTimeout (() => {
+        console.log(asciiHeart[i]);
+      }, i * ms);
+}
+}
+
+
+//Una esta comentada porque no funcionan correctamente cuando se ejecutan ambas al mismo tiempo.
+ printByChart(100);
+// printByLine(1000);
+
+
